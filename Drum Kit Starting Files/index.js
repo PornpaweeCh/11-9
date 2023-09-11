43053f2e8ce8e4777e4282b22d825687b6642890
PornpaweeCh <137771124+PornpaweeCh@users.noreply.github.com>
2023-09-11 16:26:58 +0700

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,71 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for(var i = 0;i< numberOfDrumButtons;i++)
+document.querySelectorAll("button")[i].addEventListener("click", function (){
+  var buttonInnerHTML = this.innerHTML;
+  makeSound(buttonInnerHTML);
+});
+
+document.addEventListener("keypress", function (event) {
+    makeSound(event.key);
+    
+});
+  function makeSound(key){
+  switch(key){
+    case "w" :
+    var tom1 = new Audio ("sounds/tom-1.mp3");
+    tom1.play();
+    break;
+    case "a" :
+    var tom2 = new Audio ("sounds/tom-2.mp3");
+    tom2.play();
+    break;
+    case "s" :
+    var tom3 = new Audio ("sounds/tom-3.mp3");
+    tom3.play();
+    break;
+    case "d" :
+    var tom4 = new Audio ("sounds/tom-4.mp3");
+    tom4.play();
+    break;
+    case "j" :
+    var snare = new Audio ("sounds/snare.mp3");
+    snare.play();
+    break;
+    case "k" :
+    var crash = new Audio ("sounds/crash.mp3");
+    crash.play();
+    break;
+    case "l" :
+    var kick = new Audio ("sounds/kick-bass.mp3");
+    kick.play();
+    break;
+    //console.log(this.innerHTML);
+  }
+};
+
+
+
+ 
+
+/*
+var houseKeeper1 = {
+    yearsOfExperience:12,
+    name : "Jane",
+    cleaningRepertoire:["bathroom","lobby","bedroom"]
+    
+}
+
+function houseKeeper(yearsOfExperience,name,cleaningRepertoire) {
+    this.yearsOfExperience = yearsOfExperience;
+    this.name = name;
+    this.cleaningRepertoire = cleaningRepertoire;
+    this.clean = function () {
+        alert("cleaning in progress.....");
+        
+    }
+    
+}
+var h = new houseKeeper(9, "Tom",["lobby", "bedroom"])
+h.clean()
+*/
